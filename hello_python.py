6d66245c8e7a246f97d3255934526824451a1619
onion <litianchen1989@163.com>
2015-05-16 14:45:50 +0800

    flask

--- hello_python.py
+++ hello_python.py
@@ -1,3 +1,11 @@
 __author__ = 'onion'
 
-print 'hello python'
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
